178de1c08032690f2139db2975605432830c05c6
jinzhengbe <jinzhengbe@gmail.com>
2025-06-12 09:45:16 +0900

    docs: 更新开发文档和日志 (2025-06-12)

--- backend/routes/file.go
+++ backend/routes/file.go
@@ -12,12 +12,12 @@ func SetupFileRoutes(router *gin.Engine, fileController *controllers.FileControl
 		// 公开路由
 		fileGroup.GET("/:id", fileController.GetFileDetails)
 		fileGroup.GET("/download/:id", fileController.DownloadFile)
+		fileGroup.POST("/upload", fileController.UploadFile)
 
 		// 需要认证的路由
 		authGroup := fileGroup.Group("")
 		authGroup.Use(middleware.AuthMiddleware())
 		{
-			authGroup.POST("/upload", fileController.UploadFile)
 			authGroup.POST("/batch", fileController.GetBatchFileDetails)
 			authGroup.DELETE("/:id", fileController.DeleteFile)
 			authGroup.GET("/order/:id", fileController.GetOrderFiles)
